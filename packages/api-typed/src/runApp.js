effcd975295d057e81068d0b962b3cd74bf43fac
Michal Palys-Dudek <michal@pnd.io>
2021-04-11 00:31:19 +0200

    🛠 Require reflect-metadata when running an app.

--- packages/api-typed/src/runApp.js
+++ packages/api-typed/src/runApp.js
@@ -1,4 +1,5 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+/* eslint-disable @typescript-eslint/no-var-requires */
+require('reflect-metadata');
 const { requireApp } = require('./requireApp');
 
 async function runApp(appFile, runMode) {
